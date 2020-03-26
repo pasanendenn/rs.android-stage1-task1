098ccff67303777acefdc29f87626315d0747856
Dzianis Pasanen <pasanenD@yandex.by>
2020-03-27 01:50:11 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -4,6 +4,25 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val list: ArrayList<String> = ArrayList()
+        var i = 0
+        while (i<inputString.toCharArray().size-1){
+            when (inputString[i]){
+                '<' -> if (inputString.substring(i+1, inputString.indexOf('>', i+1)).contains('<')){
+                    list.add(inputString.substring(i+1, inputString.indexOf('>', inputString.indexOf('>')+1)))
+                } else {
+                    list.add(inputString.substring(i+1, inputString.indexOf('>', i+1)))
+                }
+
+
+                '(' -> list.add(inputString.substring(i+1, inputString.indexOf(')', i+1)))
+
+                '[' -> list.add(inputString.substring(i+1, inputString.indexOf(']', i+1)))
+
+            }
+            i+=1
+        }
+        return list.toTypedArray()
     }
+
 }
