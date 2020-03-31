99cfd4f031fd728e2d8f0270e8a59636dc3cc242
Dzianis Pasanen <pasanenD@yandex.by>
2020-03-31 23:46:11 +0300

    new HighestPalindrome

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,68 +1,55 @@
 package subtask5
-
 import java.lang.StringBuilder
 
 class HighestPalindrome {
-
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-
         val newStr = StringBuilder(digitString)
+        val bool = BooleanArray(n)
         var start = 0
-        var end = n-1
-        var min = 0
+        var end = n - 1
+        var change = 0
 
-        while (end>start){
+        while (end > start){
             val sub1 = newStr[start]
             val sub2 = newStr[end]
-            if (sub1.toInt()>sub2.toInt()){
-                min+=1
-            } else if (sub1.toInt()<sub2.toInt()){
-                min+=1
+            if (sub1.toInt() > sub2.toInt()){
+                newStr[end] = sub1
+                bool[start] = true
+                change += 1
+            } else if (sub1.toInt() < sub2.toInt()){
+                newStr[start] = sub2
+                bool[start] = true
+                change += 1
             }
             start += 1
             end -= 1
         }
 
+        end = n - 1
         start = 0
-        end = n-1
 
-        if (k<min) {
-            newStr.setLength(2)
-            newStr[0] = '-'
-            newStr[1] = '1'
-        } else if (k==min){
-            while (end>start){
-                val sub1 = newStr[start]
-                val sub2 = newStr[end]
-                if (sub1.toInt()>sub2.toInt()){
-                    newStr.setCharAt(end, sub1)
-                } else if (sub1.toInt()<sub2.toInt()){
-                    newStr.setCharAt(start, sub2)
+        var diff = k - change
+        if (change > k){
+            return "-1"
+        } else if (change == k){
+            return newStr.toString()
+        } else if (change < k){
+            while (end >= start){
+                if (diff == 0) break
+                if ((bool[start] && newStr[start] != '9') || (start == end) && (newStr[start] != '9')){
+                    newStr[start] = '9'
+                    newStr[end] = '9'
+                    diff -= 1
+                } else if (!bool[start] && newStr[start] != '9' && diff > 1){
+                    newStr[start] = '9'
+                    newStr[end] = '9'
+                    diff -= 2
                 }
                 start += 1
                 end -= 1
             }
-        } else {
-            while (end>start){
-                val sub1 = newStr[start]
-                val sub2 = newStr[end]
-                if (sub1.toInt()>sub2.toInt()){
-                    newStr.setCharAt(end, sub1)
-                } else if (sub1.toInt()<sub2.toInt()){
-                    newStr.setCharAt(start, sub2)
-                }
-                start += 1
-                end -= 1
-            }
-            newStr.setCharAt(0, '9')
-            newStr.setCharAt(n-1, '9')
         }
-
         return newStr.toString()
-
     }
-    }
-
-
-
+}
